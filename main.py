ff356b79ad0ca0a12f060490226b5891de1f4518
vincent <wingedsheep89@gmail.com>
2022-11-20 16:08:35 +0100

    Rearrange ordering of dropout / layer normalization based on mingpt.

--- main.py
+++ main.py
@@ -183,7 +183,7 @@ class FeedForward(torch.nn.Module):
         return self.linear_2(torch.relu(self.linear_1(x)))
 
 
-class GptDecoderLayer(torch.nn.Module):
+class DecoderLayer(torch.nn.Module):
     """
     Pytorch module for an encoder layer.
 
@@ -206,14 +206,11 @@ class GptDecoderLayer(torch.nn.Module):
         self.feed_forward_dimension = feed_forward_dimension
         self.dropout_rate = dropout_rate
 
-        # Create the multi-headed self attention layer
         self.multi_headed_self_attention = MaskedMultiHeadedSelfAttention(embedding_dimension, number_of_heads)
-
-        # Create the feed forward layer
         self.feed_forward = FeedForward(embedding_dimension, feed_forward_dimension)
-
-        # Create the dropout layer
         self.dropout = torch.nn.Dropout(dropout_rate)
+        self.layer_normalization_1 = torch.nn.LayerNorm(embedding_dimension)
+        self.layer_normalization_2 = torch.nn.LayerNorm(embedding_dimension)
 
     def forward(self, x, mask):
         """
@@ -223,36 +220,32 @@ class GptDecoderLayer(torch.nn.Module):
         mask dimensions are: (batch_size, sequence_length)
         mask values are: 0 or 1. 0 means the token is masked, 1 means the token is not masked.
         """
-        # Compute the multi-headed self attention
-        # multi_headed_self_attention_output dimensions are: (batch_size, sequence_length, embedding_dimension)
-        multi_headed_self_attention_output = self.multi_headed_self_attention(x, mask)
 
-        # Apply dropout to the multi-headed self attention output
-        # multi_headed_self_attention_output_dropout dimensions are: (batch_size, sequence_length, embedding_dimension)
-        multi_headed_self_attention_output_dropout = self.dropout(multi_headed_self_attention_output)
+        # Layer normalization 1
+        normalized_x = self.layer_normalization_1(x)
 
-        # Add the multi-headed self attention output to the input
-        # multi_headed_self_attention_output_dropout_residual dimensions are: (batch_size, sequence_length, embedding_dimension)
-        multi_headed_self_attention_output_dropout_residual = multi_headed_self_attention_output_dropout + x
+        # Multi headed self attention
+        attention_output = self.multi_headed_self_attention(normalized_x, mask)
 
-        # Compute the feed forward layer
-        # feed_forward_output dimensions are: (batch_size, sequence_length, embedding_dimension)
-        feed_forward_output = self.feed_forward(multi_headed_self_attention_output_dropout_residual)
+        # Residual output
+        residual_output = x + attention_output
 
-        # Apply dropout to the feed forward output
-        # feed_forward_output_dropout dimensions are: (batch_size, sequence_length, embedding_dimension)
-        feed_forward_output_dropout = self.dropout(feed_forward_output)
+        # Layer normalization 2
+        normalized_residual_output = self.layer_normalization_2(residual_output)
 
-        # Add the feed forward output to the multi-headed self attention output
-        # feed_forward_output_dropout_residual dimensions are: (batch_size, sequence_length, embedding_dimension)
-        feed_forward_output_dropout_residual = feed_forward_output_dropout + multi_headed_self_attention_output_dropout_residual
+        # Feed forward
+        feed_forward_output = self.feed_forward(normalized_residual_output)
 
-        return feed_forward_output_dropout_residual
+        # Dropout
+        feed_forward_output = self.dropout(feed_forward_output)
+
+        # Residual output
+        return residual_output + feed_forward_output
 
 
-class GptDecoder(torch.nn.Module):
+class Decoder(torch.nn.Module):
     """
-    Pytorch module for the GPT-2 decoder.
+    Pytorch module for the decoder.
 
     The decoder consists of a token embedding layer, a positional encoding layer, and a stack of encoder layers.
 
@@ -278,6 +271,7 @@ class GptDecoder(torch.nn.Module):
         self.feed_forward_dimension = feed_forward_dimension
         self.dropout_rate = dropout_rate
         self.max_sequence_length = max_sequence_length
+        self.layer_normalization = torch.nn.LayerNorm(embedding_dimension)
 
         # Create the token embedding layer
         self.token_embedding = TokenEmbedding(embedding_dimension, number_of_tokens)
@@ -287,7 +281,7 @@ class GptDecoder(torch.nn.Module):
 
         # Create the encoder layers
         self.encoder_layers = torch.nn.ModuleList(
-            [GptDecoderLayer(embedding_dimension, number_of_heads, feed_forward_dimension, dropout_rate) for _ in
+            [DecoderLayer(embedding_dimension, number_of_heads, feed_forward_dimension, dropout_rate) for _ in
              range(number_of_layers)])
 
     def forward(self, x, mask):
@@ -306,9 +300,12 @@ class GptDecoder(torch.nn.Module):
         # positional_encoding dimensions are: (batch_size, sequence_length, embedding_dimension)
         positional_encoding = self.positional_encoding(token_embeddings)
 
+        # Post embedding layer normalization
+        positional_encoding_normalized = self.layer_normalization(positional_encoding)
+
         # Compute the encoder layers
         # encoder_outputs dimensions are: (batch_size, sequence_length, embedding_dimension)
-        encoder_outputs = positional_encoding
+        encoder_outputs = positional_encoding_normalized
         for encoder_layer in self.encoder_layers:
             encoder_outputs = encoder_layer(encoder_outputs, mask)
 
@@ -372,7 +369,7 @@ class GPT(torch.nn.Module):
         self.dropout_rate = dropout_rate
 
         # Create the decoder
-        self.decoder = GptDecoder(
+        self.decoder = Decoder(
             embedding_dimension,
             number_of_tokens,
             number_of_layers,
@@ -693,7 +690,7 @@ class Runner(torch.nn.Module):
         # Train the model
         optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
         trainer = Trainer(model, tokenizer, optimizer)
-        trainer.train(sequences, epochs=200, batch_size=4)
+        trainer.train(sequences, epochs=100, batch_size=4)
 
         # Generate text
         max_tokens_to_generate = 50
