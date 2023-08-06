80951abec17b06291efd59288620f3e6d6c8d14f
vincent <wingedsheep89@gmail.com>
2023-08-06 14:21:03 +0200

    Adjust naming d_model.

--- main.py
+++ main.py
@@ -9,21 +9,17 @@ import matplotlib.pyplot as plt
 
 class TokenEmbedding(torch.nn.Module):
     """
-    Pytorch module that converts tokens into embeddings.
+    PyTorch module that converts tokens into embeddings.
 
     Input dimension is: (batch_size, sequence_length)
-    Output dimension is: (batch_size, sequence_length, embedding_dimension)
+    Output dimension is: (batch_size, sequence_length, d_model)
     """
 
-    def __init__(
-            self,
-            embedding_dimension,
-            number_of_tokens
-    ):
+    def __init__(self, d_model, number_of_tokens):
         super().__init__()
         self.embedding_layer = torch.nn.Embedding(
             num_embeddings=number_of_tokens,
-            embedding_dim=embedding_dimension
+            embedding_dim=d_model
         )
 
     def forward(self, x):
